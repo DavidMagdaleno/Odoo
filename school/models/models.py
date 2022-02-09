91e1664e80d93ce547b5c4caefba99a4fa382bb6
David Magdaleno <noesis174@gmail.com>
2022-02-09 09:17:07 +0100

    v2

--- school/models/models.py
+++ school/models/models.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+
+class student(models.Model):
+     _name = 'school.student'
+     _description = 'school.student'
+
+     name = fields.Char(string="Nombre", readonly=False, required=True,help='Este es el nombre')
+     birth_year=fields.Integer()
+     description =fields.Text()
+     inscription_date=fields.Date()
+     last_login=fields.Datetime()
+     is_student=fields.Boolean()
+     photo =fields.Image(max_width=200, max_height=200)
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
 
 
 # class school(models.Model):
